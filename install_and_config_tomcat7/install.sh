8e911afcd0f3281cd134fc52007f3c399d78bfe9
advocd <advocd@gmail.com>
2014-09-27 03:30:33 -0700

    update tomcat7 script

--- install_and_config_tomcat7/install.sh
+++ install_and_config_tomcat7/install.sh
@@ -1,5 +1,5 @@
-sudo apt-get update
-sudo apt-get install tomcat7 tomcat7-admin
-sudo rm /etc/tomcat7/tomcat-user.xml
-cp tomcat-user.xml /etc/tomcat7/
+#sudo apt-get update
+#sudo apt-get install tomcat7 tomcat7-admin
+sudo rm /etc/tomcat7/tomcat-users.xml
+sudo cp tomcat-users.xml /etc/tomcat7
 sudo service tomcat7 restart
