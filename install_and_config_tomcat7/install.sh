8edd9e40aaf038f21a890c938897a400c1fee8d9
Martin Münch <advocd@gmail.com>
2014-09-27 03:55:02 -0700

    Update install.sh

--- install_and_config_tomcat7/install.sh
+++ install_and_config_tomcat7/install.sh
@@ -1,5 +1,13 @@
+echo ...update sources...
 sudo apt-get update
+echo ...install tomcat 7 and web-ui
 sudo apt-get install tomcat7 tomcat7-admin
+echo ...batch tomcat 7 user config
 sudo rm /etc/tomcat7/tomcat-users.xml
 sudo cp tomcat-users.xml /etc/tomcat7
 sudo service tomcat7 restart
+echo ""
+echo ################################
+echo user: admin
+echo pass: kickstart
+echo ################################
