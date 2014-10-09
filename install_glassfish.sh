cede1edd155edbaf6f041480089599d6e811d9ae
Martin Münch <advocd@gmail.com>
2014-10-09 23:23:32 +0200

    Update install_glassfish.sh

--- install_glassfish.sh
+++ install_glassfish.sh
@@ -1,5 +1,5 @@
 sudo apt-get install python-software-properties -y
-sudo add-apt-repository ppa:webupd8team/java -y
+sudo add-apt-repository ppa:webupd8team/java 
 sudo apt-get update
 sudo apt-get install oracle-java7-installer -y
 
