186222b02c13ee28f397d278f75ce829df9529ba
Martin Münch <advocd@gmail.com>
2014-10-09 23:17:57 +0200

    Create install_glassfish.sh

---
+++ install_glassfish.sh
@@ -0,0 +1,11 @@
+sudo apt-get install python-software-properties -y
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update
+sudo apt-get install oracle-java7-installer -y
+
+cd ~/Downloads
+wget download.java.net/glassfish/4.0/release/glassfish-4.0.zip
+sudo apt-get install unzip -y
+sudo unzip glassfish-4.0.zip -d /opt
+
+sudo ./asadmin start-domain
